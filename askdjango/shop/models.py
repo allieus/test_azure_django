bd7dd8567a72a8e9e12045dd20067ff1a711a58b
Chinseok Lee <allieuslee@gmail.com>
2016-05-05 12:07:32 +0900

    shop 앱을 프로젝트에 등록하고 Product 모델 생성

--- askdjango/shop/models.py
+++ askdjango/shop/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.PositiveIntegerField()
+    desc = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
