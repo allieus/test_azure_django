418c244a1759a2e816e251ab47bf227c3d9cc052
Chinseok Lee <allieuslee@gmail.com>
2016-05-05 16:27:32 +0900

    shop:product_edit 뷰 구현

--- askdjango/shop/urls.py
+++ askdjango/shop/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     url(r'^$', views.product_list, name='product_list'),
     url(r'^(?P<pk>\d+)/$', views.product_detail, name='product_detail'),
     url(r'^new/$', views.product_new, name='product_new'),
+    url(r'^(?P<pk>\d+)/edit/$', views.product_edit, name='product_edit'),
 ]
--- askdjango/shop/views.py
+++ askdjango/shop/views.py
@@ -3,6 +3,7 @@ from django.core.urlresolvers import reverse_lazy
 from django.views.generic import ListView
 from django.views.generic import DetailView
 from django.views.generic import CreateView
+from django.views.generic import UpdateView
 from .models import Product
 from .forms import ProductForm
 
@@ -13,3 +14,5 @@ product_detail = DetailView.as_view(model=Product)
 
 product_new = CreateView.as_view(model=Product, form_class=ProductForm)
     # success_url=reverse_lazy('shop:product_list'))
+
+product_edit = UpdateView.as_view(model=Product, form_class=ProductForm)
